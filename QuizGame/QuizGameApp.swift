cbdc1277f4131f3312186b11821c3f31ce4b29e0
Ioannis Gimiliaris <johngimis@gmail.com>
2023-05-08 21:33:19 +0100

    Initial Commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by John Gimiliaris on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
